839e08d11d8d7ae7105eb9ed753db0652c5f5180
Richard <wei.richard.s@gmail.com>
2019-09-05 15:46:34 -0700

    add component files

---
+++ src/ImeBubble.js
@@ -0,0 +1,11 @@
+import	React, {Component} from 'react';
+
+class	ImeBubble extends Component {
+	render() {
+		return (
+			<div>Hello World!</div>
+		)
+	}
+};
+
+export default ImeBubble;
---
+++ src/Words.js
@@ -0,0 +1,11 @@
+import	React, {Component} from 'react';
+
+class	Words extends Component {
+	render() {
+		return (
+			<div>Hello World!</div>
+		)
+	}
+};
+
+export default Words;
